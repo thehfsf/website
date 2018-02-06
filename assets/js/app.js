37cded85e25bca5770f3335a2525d45cefe2f012
Marko Alerić <markoall1@gmail.com>
2018-02-06 23:05:23 +0100

    Added template

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).foundation();
+//@prepros-prepend ../../foundation/bower_components/jquery/dist/jquery.min.js
+//@prepros-prepend ../../foundation/bower_components/what-input/what-input.min.js
+//@prepros-prepend ../../foundation/bower_components/foundation-sites/dist/foundation.min.js
+//@prepros-append images-loaded.min.js
+//@prepros-append litebox.min.js
+//@prepros-append slick.min.js
+//@prepros-append jquery.slicknav.min.js
+//@prepros-append responsive-tables.js
---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+// CHANGE THE EMAIL WHERE THE FORM WILL BE DELIVERED HERE
+define("WEBMASTER_EMAIL", 'your.email.here@mail.com');
+?>
